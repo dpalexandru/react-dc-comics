cba7959d8ba9fce2f14a15cb201958b0200076e5
dpalexandru <alexpoian91@hotmail.com>
2025-08-25 15:23:09 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="logo"></div>
+      <div className="menuHeader"></div>
+
+    </header>
+  )
+}
+
+export default Header
