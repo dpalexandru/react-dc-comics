cf4568460f51e83adc098a4614cfb5d46c06eae0
dpalexandru <alexpoian91@hotmail.com>
2025-08-25 15:40:01 +0200

    feat: add logo and ul to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header>
-      <div className="logo">Ciao</div>
-      <div className="menuHeader"></div>
+    <header className='container'>
+      <div className="logo">
+        <img src="./img/dc-logo.png" alt="logo" />
+      </div>
+      <div className="menuHeader">
+        <ul id="menuHeader">
+          <li>CHARACTERS</li>
+          <li>COMICS</li>
+          <li>MOVIES</li>
+          <li>TV</li>
+          <li>GMES</li>
+          <li>COLLECTIBLES</li>
+          <li>VIDEOS</li>
+          <li>FANS</li>
+          <li>NEWS</li>
+          <li>SHOP</li>
+        </ul>
+      </div>
 
     </header>
   )
