6137c67667bcb572550049321ee19860b782537e
dpalexandru <alexpoian91@hotmail.com>
2025-08-25 18:06:09 +0200

    fix: correct position to dc logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -35,52 +35,54 @@ const Footer = () => {
       {/* Sezione footer-info */}
 
       <div className="footer-info-container">
-        <div class="container footer-info">
-          <div class="footer-col">
-            <h4>DC COMICS</h4>
-            <ul>
-              <li><a href="#">Characters</a></li>
-              <li><a href="#">Comics</a></li>
-              <li><a href="#">Movies</a></li>
-              <li><a href="#">TV</a></li>
-              <li><a href="#">Games</a></li>
-              <li><a href="#">Videos</a></li>
-              <li><a href="#">News</a></li>
-            </ul>
+        <div className="container footer-info">
+          <div className="col-info-left">
+            <div className="footer-col">
+              <h4>DC COMICS</h4>
+              <ul>
+                <li><a href="#">Characters</a></li>
+                <li><a href="#">Comics</a></li>
+                <li><a href="#">Movies</a></li>
+                <li><a href="#">TV</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Videos</a></li>
+                <li><a href="#">News</a></li>
+              </ul>
 
-            <h4>SHOP</h4>
-            <ul>
-              <li><a href="#">Shop DC</a></li>
-              <li><a href="#">Shop DC Collectibles</a></li>
-            </ul>
-          </div>
+              <h4>SHOP</h4>
+              <ul>
+                <li><a href="#">Shop DC</a></li>
+                <li><a href="#">Shop DC Collectibles</a></li>
+              </ul>
+            </div>
 
-          <div class="footer-col">
-            <h4>DC</h4>
-            <ul>
-              <li><a href="#">Terms Of Use</a></li>
-              <li><a href="#">Privacy policy (New)</a></li>
-              <li><a href="#">Ad Choices</a></li>
-              <li><a href="#">Advertising</a></li>
-              <li><a href="#">Jobs</a></li>
-              <li><a href="#">Subscriptions</a></li>
-              <li><a href="#">Talent Workshops</a></li>
-              <li><a href="#">CPSC Certificates</a></li>
-              <li><a href="#">Ratings</a></li>
-              <li><a href="#">Shop Help</a></li>
-              <li><a href="#">Contact Us</a></li>
-            </ul>
-          </div>
+            <div className="footer-col">
+              <h4>DC</h4>
+              <ul>
+                <li><a href="#">Terms Of Use</a></li>
+                <li><a href="#">Privacy policy (New)</a></li>
+                <li><a href="#">Ad Choices</a></li>
+                <li><a href="#">Advertising</a></li>
+                <li><a href="#">Jobs</a></li>
+                <li><a href="#">Subscriptions</a></li>
+                <li><a href="#">Talent Workshops</a></li>
+                <li><a href="#">CPSC Certificates</a></li>
+                <li><a href="#">Ratings</a></li>
+                <li><a href="#">Shop Help</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+            </div>
 
-          <div class="footer-col">
-            <h4>SITES</h4>
-            <ul>
-              <li><a href="#">DC</a></li>
-              <li><a href="#">MAD Magazine</a></li>
-              <li><a href="#">DC Kids</a></li>
-              <li><a href="#">DC Universe</a></li>
-              <li><a href="#">DC Power Visa</a></li>
-            </ul>
+            <div className="footer-col">
+              <h4>SITES</h4>
+              <ul>
+                <li><a href="#">DC</a></li>
+                <li><a href="#">MAD Magazine</a></li>
+                <li><a href="#">DC Kids</a></li>
+                <li><a href="#">DC Universe</a></li>
+                <li><a href="#">DC Power Visa</a></li>
+              </ul>
+            </div>
           </div>
 
           <div className="bigLogo">
@@ -90,7 +92,7 @@ const Footer = () => {
         </div>
       </div>
 
-      {/* Sezione footer-button */}
+      {/* Sezione footer-buttom */}
       <div className="footer-buttom">
         <div className="container">
           <div className="button">
