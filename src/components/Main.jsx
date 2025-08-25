2e711fdf4d87af746587d056d6cc8eb2a6e39aa8
dpalexandru <alexpoian91@hotmail.com>
2025-08-25 16:12:35 +0200

    feat: add main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <main>
+      <div className="container">
+        <p id='content'>_ _&gt; Content goes here &lt;_ _</p>
+      </div>
+    </main>
+  )
+}
+
+export default Main
