175f2d513db10086b1c275fae5ccbf8198fec321
dpalexandru <alexpoian91@hotmail.com>
2025-08-26 17:04:12 +0200

    feat: bonus 1 create a bluebar component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header"
 import Main from "./components/Main"
 import Footer from "./components/Footer"
 import Jumbotron from "./components/Jumbotron"
+import BlueBar from "./components/BlueBar"
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Jumbotron />
       <Main />
+      <BlueBar />
       <Footer />
     </>
   )
