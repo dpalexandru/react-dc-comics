a07b1ef934bc87a9eabaf2adc7b71cab4b730db4
dpalexandru <alexpoian91@hotmail.com>
2025-08-26 15:17:48 +0200

    feat: add jumbotron component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header"
 import Main from "./components/Main"
 import Footer from "./components/Footer"
+import Jumbotron from "./components/Jumbotron"
 
 function App() {
 
   return (
     <>
       <Header />
+      <Jumbotron />
       <Main />
       <Footer />
     </>
