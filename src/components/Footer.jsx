8f70304881f06af4939731cfe725151726bd1a3b
dpalexandru <alexpoian91@hotmail.com>
2025-08-27 16:08:19 +0200

    feat: bonus 3: move all data of  header, cards, and footer to app.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,60 +1,8 @@
 import React from 'react';
 
 
-{/* Creo array con oggetti dentro */ }
-const footerInfo = [
-  {
-    id: 1,
-    title: "DC COMICS",
-    links: [
-      "Characters",
-      "Comics",
-      "Movies",
-      "TV",
-      "Games",
-      "Videos",
-      "News"
-    ]
-  },
-  {
-    id: 2,
-    title: "SHOP",
-    links: [
-      "Shop DC",
-      "Shop DC Collectibles"
-    ]
-  },
-  {
-    id: 3,
-    title: "DC",
-    links: [
-      "Terms Of Use",
-      "Privacy policy (New)",
-      "Ad Choices",
-      "Advertising",
-      "Jobs",
-      "Subscriptions",
-      "Talent Workshops",
-      "CPSC Certificates",
-      "Ratings",
-      "Shop Help",
-      "Contact Us"
-    ]
-  },
-  {
-    id: 4,
-    title: "SITES",
-    links: [
-      "DC",
-      "MAD Magazine",
-      "DC Kids",
-      "DC Universe",
-      "DC Power Visa"
-    ]
-  }
-];
 
-const Footer = () => {
+const Footer = ({ footerInfo }) => {
   return (
     <footer>
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,8 @@
 import React from 'react'
 
-{/*Creo array di ogetti Items */ }
-const menuItems = [
-  { id: 1, href: "#", title: "CHARACTERS" },
-  { id: 2, href: "#", title: "COMICS" },
-  { id: 3, href: "#", title: "MOVIES" },
-  { id: 4, href: "#", title: "TV" },
-  { id: 5, href: "#", title: "GAMES" },
-  { id: 6, href: "#", title: "COLLECTIBLES" },
-  { id: 7, href: "#", title: "VIDEOS" },
-  { id: 8, href: "#", title: "FANS" },
-  { id: 9, href: "#", title: "NEWS" },
-  { id: 10, href: "#", title: "SHOP" },
-];
 
-const Header = () => {
+
+const Header = ({ menuItems }) => {
 
   return (
     <header className='container'>
